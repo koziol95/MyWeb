edd4b892546360813f1863d463067c087cee59d9
Pav <121645352+koziol95@users.noreply.github.com>
2024-02-26 17:09:48 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__, static_url_path='/static')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
